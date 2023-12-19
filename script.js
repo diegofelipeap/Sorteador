8b6b30b896b2fb8d27a7f862c1377609470c226d
diegofelipeap <diegospd22@gmail.com>
2023-12-18 22:57:53 -0300

    Projeto de Sorteador profissional entre qualquer valor.

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value) // valor do input "entre"
+    const max = Math.floor(document.querySelector(".input-max").value) // valor do input "e"
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Exibindo o resultado no <h2>
+    document.querySelector('h2').innerHTML = `Número sorteado:<b> ${result} </b>`;
+}
