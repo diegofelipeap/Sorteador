5bc0470e8482a9171cef1b5ac406c8584a672d28
diegofelipeap <diegospd22@gmail.com>
2024-01-01 23:54:18 -0300

    Implementando sorteador de nomes.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+function showNumberInputs() {
+    document.getElementById('numberInputs').style.display = 'block';
+    document.getElementById('nameTextarea').style.display = 'none';
+}
+
+function showNameTextarea() {
+    document.getElementById('nameTextarea').style.display = 'block';
+    document.getElementById('numberInputs').style.display = 'none';
+}
+
 function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value) // valor do input "entre"
     const max = Math.floor(document.querySelector(".input-max").value) // valor do input "e"
@@ -14,9 +25,33 @@ function generateNumber() {
         return;
     }
 
-    else{
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    // Exibindo o resultado no <h2>
-    document.querySelector('h2').innerHTML = `Número sorteado:<b> ${result} </b>`;
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        // Exibindo o resultado no <h2>
+        document.querySelector('h2').innerHTML = `Número sorteado:<b> ${result} </b>`;
+    }
 }
+
+function generateName() {
+
+
+    const namesInput = document.querySelector(".input-names").value.trim(); // Pega os nomes inseridos
+    const namesArray = namesInput.split(','); // Converte a string em um array de nomes
+
+    // Remove espaços em branco adicionais em cada nome e filtra nomes vazios
+    const validNames = namesArray.map(name => name.trim()).filter(name => name !== "");
+
+    // Verifica se há nomes válidos
+    if (validNames.length === 0) {
+        document.querySelector('h2').textContent = `Digite pelo menos um nome válido`;
+        return;
+    }
+
+    // Sorteia um nome aleatório
+    const randomIndex = Math.floor(Math.random() * validNames.length);
+    const randomName = validNames[randomIndex];
+
+    // Exibe o nome sorteado
+    document.querySelector('h2').innerHTML = `Nome sorteado: <b>${randomName}</b>`;
+
 }
